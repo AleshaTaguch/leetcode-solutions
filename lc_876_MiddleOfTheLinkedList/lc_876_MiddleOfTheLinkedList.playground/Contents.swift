f11a754abfd55d31d1c669a70423da47ccbe76d7
AleshaTaguch <alesha.taguch@gmail.com>
2023-06-09 01:59:13 +0500

    lc_876_MiddleOfTheLinkedList result

--- lc_876_MiddleOfTheLinkedList/lc_876_MiddleOfTheLinkedList.playground/Contents.swift
+++ lc_876_MiddleOfTheLinkedList/lc_876_MiddleOfTheLinkedList.playground/Contents.swift
@@ -1,67 +1,103 @@
 import UIKit
 
 /**
- https://leetcode.com/problems/fizz-buzz/description/
- Given an integer n, return a string array answer (1-indexed) where:
+ https://leetcode.com/problems/middle-of-the-linked-list/
+ Given the head of a singly linked list, return the middle node of the linked list.
 
- answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
- answer[i] == "Fizz" if i is divisible by 3.
- answer[i] == "Buzz" if i is divisible by 5.
- answer[i] == i (as a string) if none of the above conditions are true.
+ If there are two middle nodes, return the second middle node.
  
  Example 1:
+ Input: head = [1,2,3,4,5]
+ Output: [3,4,5]
+ Explanation: The middle node of the list is node 3.
 
- Input: n = 3
- Output: ["1","2","Fizz"]
  Example 2:
+ Input: head = [1,2,3,4,5,6]
+ Output: [4,5,6]
+ Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+  
 
- Input: n = 5
- Output: ["1","2","Fizz","4","Buzz"]
- Example 3:
+ Constraints:
 
- Input: n = 15
- Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ The number of nodes in the list is in the range [1, 100].
+ 1 <= Node.val <= 100
  
 */
 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+
 class Solution {
-    func fizzBuzz(_ n: Int) -> [String] {
-        var resArray: [String] = []
-        var dev3: Int
-        var dev5: Int
-        for index in 1...n {
-            dev3 = index % 3
-            dev5 = index % 5
-            if dev3 == 0 && dev5 == 0  {
-                resArray.append("FizzBuzz")
-            } else if dev3 == 0 {
-                resArray.append("Fizz")
-            } else if dev5 == 0 {
-                resArray.append("Buzz")
-            } else {
-                resArray.append(String(index))
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        var currentNode: ListNode?
+        var resultNode: ListNode?
+
+        currentNode = headNode
+        resultNode = headNode
+        while currentNode != nil {
+            if let uwCurrentNode1 = currentNode {
+                currentNode = uwCurrentNode1.next
+                if let uwResultNode = resultNode, let uwCurrentNode2 = currentNode {
+                    resultNode = uwResultNode.next
+                    currentNode = uwCurrentNode2.next
+                }
             }
+            
         }
         
-        return resArray
+        return resultNode
     }
 }
 
-let solution = Solution()
-print(solution.fizzBuzz(3))
-print(solution.fizzBuzz(5))
-print(solution.fizzBuzz(15))
+let nodeArray: [Int] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+var headNode: ListNode?
 
-
-
-var current = head
-  var array = [Int]()
-  
-  while let node = current {
-      array.append(node.val)
-      current = node.next
-  }
-
-  return array == array.reversed()
+for item in nodeArray.reversed() {
+    print(item)
+    headNode = ListNode(item,headNode)
 }
+
+var currentNode: ListNode?
+var resultNode: ListNode?
+
+print("----------")
+currentNode = headNode
+resultNode = headNode
+while currentNode != nil {
+    if let uwCurrentNode1 = currentNode {
+        print(uwCurrentNode1.val)
+        currentNode = uwCurrentNode1.next
+        if let uwResultNode = resultNode, let uwCurrentNode2 = currentNode {
+            resultNode = uwResultNode.next
+            currentNode = uwCurrentNode2.next
+        }
+    }
+    
+}
+print("----------")
+print(resultNode?.val)
+print("----------")
+
+let testSolution: Solution = Solution()
+let resTest: ListNode? = testSolution.middleNode(headNode)
+print(resTest?.val)
 
