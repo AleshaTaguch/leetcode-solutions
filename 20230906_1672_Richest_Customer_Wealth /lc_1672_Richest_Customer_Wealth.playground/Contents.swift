3b63af7ac851d88a1a278948ffb83c5f76dd794b
AleshaTaguch <alesha.taguch@gmail.com>
2023-10-29 23:41:15 +0500

    20230906_1672_Richest_Customer_Wealth done

--- 20230906_1672_Richest_Customer_Wealth /lc_1672_Richest_Customer_Wealth.playground/Contents.swift
+++ 20230906_1672_Richest_Customer_Wealth /lc_1672_Richest_Customer_Wealth.playground/Contents.swift
@@ -1,86 +1,50 @@
 import UIKit
 
 /**
- https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/?envType=featured-list&envId=challenges-for-new-users
- 1342. Number of Steps to Reduce a Number to Zero
+ https://leetcode.com/problems/richest-customer-wealth
+ 1672. Richest Customer Wealth
+ You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
 
- Given an integer num, return the number of steps to reduce it to zero.
-
- In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
-
+ A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+ 
  Example 1:
 
- Input: num = 14
+ Input: accounts = [[1,2,3],[3,2,1]]
  Output: 6
  Explanation:
- Step 1) 14 is even; divide by 2 and obtain 7.
- Step 2) 7 is odd; subtract 1 and obtain 6.
- Step 3) 6 is even; divide by 2 and obtain 3.
- Step 4) 3 is odd; subtract 1 and obtain 2.
- Step 5) 2 is even; divide by 2 and obtain 1.
- Step 6) 1 is odd; subtract 1 and obtain 0.
+ 1st customer has wealth = 1 + 2 + 3 = 6
+ 2nd customer has wealth = 3 + 2 + 1 = 6
+ Both customers are considered the richest with a wealth of 6 each, so return 6.
  Example 2:
 
- Input: num = 8
- Output: 4
+ Input: accounts = [[1,5],[7,3],[3,5]]
+ Output: 10
  Explanation:
- Step 1) 8 is even; divide by 2 and obtain 4.
- Step 2) 4 is even; divide by 2 and obtain 2.
- Step 3) 2 is even; divide by 2 and obtain 1.
- Step 4) 1 is odd; subtract 1 and obtain 0.
+ 1st customer has wealth = 6
+ 2nd customer has wealth = 10
+ 3rd customer has wealth = 8
+ The 2nd customer is the richest with a wealth of 10.
  Example 3:
 
- Input: num = 123
- Output: 12
+ Input: accounts = [[2,8,7],[7,1,3],[1,9,5]]
+ Output: 17
   
 
  Constraints:
 
- 0 <= num <= 106
- 
-*/
-
-/* лучший вариант
- class Solution {
-     func numberOfSteps(_ num: Int) -> Int {
-         if num == 1 {
-             return 1
-         }
-         if num == 0 {
-             return 0
-         }
-         
-         var currValue = num
-         if abs(Float(currValue).remainder(dividingBy: 2)) == 1 {
-             currValue -= 1
-         } else {
-             currValue /= 2
-         }
-         let result = numberOfSteps(currValue)
-         
-         return result + 1
-     }
- }
+ m == accounts.length
+ n == accounts[i].length
+ 1 <= m, n <= 50
+ 1 <= accounts[i][j] <= 100
  */
-class Solution {
-    func numberOfSteps(_ num: Int) -> Int {
-        var numValue: Int = num
-        var countStep: Int = 0
 
-        while numValue > 0 {
-            if numValue % 2 == 0 {
-                numValue = numValue / 2
-            } else {
-                numValue = numValue - 1
-            }
-            countStep += 1
-        }
-        return countStep
+class Solution {
+    func maximumWealth(_ accounts: [[Int]]) -> Int {
+        return accounts.map { customs in customs.reduce(0, { sumWealth, wealth in sumWealth + wealth }) }.max() ?? 0
     }
 }
 
+let solution: Solution = Solution()
+var case1: [[Int]] = [[1,2,3,5],[3,2,1,4],[3,2,3,7],[10,2,1]]
 
-//let solution: Solution = Solution()
-//print(solution.numberOfSteps(123))
-
-print("test")
+print(solution.maximumWealth(case1))
